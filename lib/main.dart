f27b715a289b227c08d36876f4ee36f9564e2ea5
Elmedin Arifi <elmedinarifi2000@gmail.com>
2024-05-08 11:18:32 +0200

    first changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,52 +2,43 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main()
-{
-    runApp(MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
-    const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-    @override
-    Widget build(BuildContext context)
-    {
-        return ChangeNotifierProvider(
-            create: (context) => MyAppState(),
-            child: MaterialApp(
-                title: 'Hello, World!',
-                theme: ThemeData(
-                    useMaterial3: true,
-                    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-                ),
-                home: MyHomePage(),        
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Hello, World!',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
 }
 
-class MyAppState extends ChangeNotifier
-{
-    var current = WordPair.random();
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
 }
 
-class MyHomePage extends StatelessWidget
-{
-    @override
-    Widget build(BuildContext context)
-    {
-        var appState = context.watch<MyAppState>();
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
 
-        return Scaffold(
-            body: Column(
-                children: 
-                [
-                    Text("A random idea:"),
-                    Text(appState.current.asLowerCase),
-                ]
-            ),
-        );
-    }
+    return Scaffold(
+      body: Column(children: [
+        Text("Mart is gangster"),
+        Text(appState.current.asLowerCase),
+      ]),
+    );
+  }
 }
