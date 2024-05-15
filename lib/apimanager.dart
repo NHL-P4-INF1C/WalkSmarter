71d85249aac6e37afa6799cd0c571f8b0400d886
mart-Velema <mart.velema@student.nhlstenden.com>
2024-05-16 00:37:12 +0200

    feat: Added HTTP functionality

---
+++ lib/apimanager.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future <http.Response> sendRequest(
+  Map<String, dynamic> payload, 
+  String url,
+  {bool isDevBranch = false}
+) async
+{
+  String devBranchUrl = isDevBranch ? '/dev_env/' : '/';
+  //TODO Make this a global var or something
+  devBranchUrl = 'http://192.168.178.65:3000$devBranchUrl$url';
+  try 
+  {
+    return await http.post(
+      Uri.parse(devBranchUrl),
+      //Setting up the header and body as a key-value array
+      headers: <String, String>
+      {
+        'Content-Type': 'application/json'
+      },
+      body: jsonEncode(<String, dynamic>
+      {
+        'payload': payload
+      }),
+      //If the request takes longer than 5 seconds, throw a timeout error
+    ).timeout(const Duration(seconds: 5));
+  } 
+  catch (e) 
+  {
+    return http.Response('e', 500);
+  }
+}
+
+class RequestManager
+{
+  bool isDevBranch;
+  String url;
+  late Map<String, dynamic> payload;
+  late Map<String, dynamic> output =
+  {
+    'response': 0
+  };
+
+  RequestManager(
+    Map<String, dynamic> defaultState, 
+    Map<String, dynamic> payloadData, 
+    this.url, 
+    {
+      this.isDevBranch = false
+    }
+  )
+  {
+    payload = payloadData; 
+    output['response'] = defaultState;
+  }
+
+  Future<Map<String, dynamic>> makeApiCall() async
+  { 
+    await updateStatus();
+    return output;
+  }
+
+  Map<String, dynamic> getCurrentOutput()
+  {
+    return output;
+  }
+
+  Future<void> updateStatus() async
+  {
+    http.Response response = await sendRequest(payload, url, isDevBranch: isDevBranch);
+    String jsonString = response.body; 
+    print(response.body);
+    if (response.statusCode == 200) 
+    {
+      try 
+      {
+        output = jsonDecode(jsonString);
+      } 
+      catch (e) 
+      {
+        output['response'] = "Failed to decode package: ${e.toString()}";
+      }
+    } 
+    else 
+    {
+      print(response.statusCode);
+      output['response'] = "Failed to connect to API. status code: ${response.statusCode}";
+    }
+    print(output);
+  }
+}
+
+//Temp frontend page. Everything below this comment is just example/ test code
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello, World!"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SelectionButton(
+                {'test': 'HELLO, wORLD!'}, 
+                {'test': 1}, 
+                'param_test')
+              ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
+//This is an example button that makes an HTTP request using the RequestManager class
+
+class SelectionButton extends StatefulWidget
+{
+  late final RequestManager requestManager;
+
+  SelectionButton(    
+    Map<String, dynamic> defaultState,
+    Map<String, dynamic> payloadData,
+    String url,
+    {
+      isDevBranch = false, 
+      super.key
+    }
+  )
+  {
+    requestManager = RequestManager(
+      defaultState,
+      payloadData,
+      url,
+      isDevBranch: isDevBranch
+    );
+  }
+
+  @override
+  State<SelectionButton> createState() => _SelectionButtonState();
+}
+
+class _SelectionButtonState extends State<SelectionButton>
+{
+  bool _requestMade = false;
+  late Map<String, dynamic> _response;
+
+  @override
+  Widget build(BuildContext context)
+  {
+    //This is cursed... but it works :3
+    return ElevatedButton(
+      onPressed: () async {
+        //Wait for the RequestManager to finish it's makeApiCall() function before going on
+        _response = await widget.requestManager.makeApiCall();
+        setState(() {
+          _requestMade = true;
+        });
+      },
+      child: Text(
+        !_requestMade 
+          //Default message setup in the constructor of the button
+          ? widget.requestManager.getCurrentOutput()['response']['test'].toString()
+          //Output of the API
+          : _response['response'].toString(),
+      ),
+    );
+  }
+}
