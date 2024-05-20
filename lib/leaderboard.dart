727d674c31f957e151c7a34600d2bac3559456ba
bryan potze <73696184+BryanPotze@users.noreply.github.com>
2024-05-20 21:58:18 +0200

    feat: added some marges and added a dropdown menu with all the months of the year

--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -9,16 +9,21 @@ class LeaderboardPage extends StatefulWidget {
 
 class _LeaderboardPageState extends State<LeaderboardPage> {
   int _selectedIndex = 1;
+  late String _selectedMonth;
 
   @override
   void initState() {
     super.initState();
     initializeDateFormatting('nl', null);
+    _selectedMonth = DateFormat.MMMM('nl').format(DateTime.now());
   }
 
   @override
   Widget build(BuildContext context) {
-    String currentMonth = DateFormat.MMMM('nl').format(DateTime.now());
+    List<String> months = [
+      'januari', 'februari', 'maart', 'april', 'mei', 'juni',
+      'juli', 'augustus', 'september', 'oktober', 'november', 'december'
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -59,124 +64,126 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
           ),
         ],
       ),
-body: Column(
-  crossAxisAlignment: CrossAxisAlignment.start,
-  children: [
-    Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            'Leaderboard',
-            style: TextStyle(fontSize: 20),
-          ),
-          SizedBox(width: 8),
-          Text(
-            currentMonth,
-            style: TextStyle(color: Color.fromRGBO(9, 106, 46, 1), fontSize: 20),
-          ),
-          Spacer(),
-          ElevatedButton(
-            onPressed: () {
-              // Add functionality for the button here
-            },
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Color.fromRGBO(9, 106, 46, 1)),
-              shape: MaterialStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Text(
+                  'Leaderboard',
+                  style: TextStyle(fontSize: 20),
                 ),
-              ),
+                SizedBox(width: 8),
+                DropdownButton<String>(
+                  value: _selectedMonth,
+                  icon: Icon(Icons.arrow_drop_down),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: TextStyle(
+                    color: Color.fromRGBO(9, 106, 46, 1),
+                    fontSize: 20,
+                  ),
+                  underline: Container(
+                    height: 2,
+                    color: Color.fromRGBO(9, 106, 46, 1),
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedMonth = newValue!;
+                    });
+                  },
+                  items: months.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ],
             ),
-            child: Text(
-              'Selecteer maand',
-              style: TextStyle(color: Colors.white, fontSize: 14),
+          ),
+          Expanded(
+            child: ListView.builder(
+              physics: AlwaysScrollableScrollPhysics(),
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                String position;
+                String emoji;
+                switch (index) {
+                  case 0:
+                    position = '1';
+                    emoji = '🏆';
+                    break;
+                  case 1:
+                    position = '2';
+                    emoji = '🥈';
+                    break;
+                  case 2:
+                    position = '3';
+                    emoji = '🥉';
+                    break;
+                  default:
+                    position = '${index + 1}';
+                    emoji = '';
+                }
+                return Container(
+                  decoration: BoxDecoration(
+                    // border: Border(
+                    //   top: BorderSide(color: Colors.grey),
+                    //   bottom: BorderSide(color: Colors.grey),
+                    // ),
+                  ),
+                  child: ListTile(
+                    title: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 25, // Set a fixed width for the ranking column
+                          child: Text(
+                            position,
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ), // Display the position
+                        SizedBox(width: 8), // Add spacing between ranking and profile picture
+                        CircleAvatar(
+                          radius: 20, // Adjust the radius as needed
+                          child: Icon(Icons.account_circle, size: 40), // Use Icon widget instead of icon property
+                        ),
+                        SizedBox(width: 8), // Add spacing between profile picture and name
+                        Text(
+                          'Gebruikersnaam',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                        ), // Replace with actual name
+                        SizedBox(width: 4), // Add spacing between name and emoji
+                        Text(
+                          emoji,
+                          style: TextStyle(fontSize: 20),
+                        ), // Replace with actual emoji
+                        Spacer(), // Add spacer to push points to the end
+                        Text(
+                          '1000',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ), // Replace with actual points
+                        SizedBox(width: 4), // Add spacing between points and 'Points'
+                        Text(
+                          'Punten',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ],
       ),
-    ),
-    Expanded(
-      child: ListView.builder(
-        physics: AlwaysScrollableScrollPhysics(),
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          String position;
-          String emoji;
-          switch (index) {
-            case 0:
-              position = '1';
-              emoji = '🏆';
-              break;
-            case 1:
-              position = '2';
-              emoji = '🥈';
-              break;
-            case 2:
-              position = '3';
-              emoji = '🥉';
-              break;
-            default:
-              position = '${index + 1}';
-              emoji = '';
-          }
-          return Container(
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(color: Colors.grey),
-                bottom: BorderSide(color: Colors.grey),
-              ),
-            ),
-            child: ListTile(
-              title: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 50, // Set a fixed width for the ranking column
-                    child: Text(
-                      position,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ), // Display the position
-                    SizedBox(width: 8), // Add spacing between ranking and profile picture
-                    CircleAvatar(
-                      radius: 20, // Adjust the radius as needed
-                      child: Icon(Icons.account_circle, size: 40), // Use Icon widget instead of icon property
-                    ),
-                  SizedBox(width: 8), // Add spacing between profile picture and name
-                  Text(
-                    'Gebruikersnaam',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ), // Replace with actual name
-                  SizedBox(width: 4), // Add spacing between name and emoji
-                  Text(
-                    emoji,
-                    style: TextStyle(fontSize: 20),
-                  ), // Replace with actual emoji
-                  Spacer(), // Add spacer to push points to the end
-                  Text(
-                    '1000',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ), // Replace with actual points
-                  SizedBox(width: 4), // Add spacing between points and 'Points'
-                  Text(
-                    'Punten',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-    ),
-  ],
-),
-
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
           color: Colors.white,
