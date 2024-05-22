bea5e9900b35e825c9b7528f320b7cd9c6f68e7b
bryan potze <bryan.potze@student.nhlstenden.com>
2024-05-22 11:58:23 +0200

    Feat: changes bottom navigation bar

--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -67,27 +67,28 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
+          Container(
+            color: Color.fromRGBO(9, 106, 46, 1), // Green background color
             padding: const EdgeInsets.all(8.0),
             child: Row(
               children: [
                 Text(
                   'Leaderboard',
-                  style: TextStyle(fontSize: 20),
+                  style: TextStyle(fontSize: 20, color: Colors.white), // White text color
                 ),
                 SizedBox(width: 8),
                 DropdownButton<String>(
                   value: _selectedMonth,
-                  icon: Icon(Icons.arrow_drop_down),
+                  icon: Icon(Icons.arrow_drop_down, color: Colors.white),
                   iconSize: 24,
-                  elevation: 16,
+                  elevation: 0,
                   style: TextStyle(
                     color: Color.fromRGBO(9, 106, 46, 1),
                     fontSize: 20,
                   ),
                   underline: Container(
                     height: 2,
-                    color: Color.fromRGBO(9, 106, 46, 1),
+                    color: Colors.white, // White underline color
                   ),
                   onChanged: (String? newValue) {
                     setState(() {
@@ -97,13 +98,17 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
                   items: months.map<DropdownMenuItem<String>>((String value) {
                     return DropdownMenuItem<String>(
                       value: value,
-                      child: Text(value),
+                      child: Text(
+                        value,
+                        style: TextStyle(color: Colors.white), // White dropdown text color
+                      ),
                     );
                   }).toList(),
                 ),
               ],
             ),
           ),
+
           Container(
             height: 200,
             decoration: BoxDecoration(
@@ -119,19 +124,19 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
               ],
             ),
             child: Stack(
-              alignment: Alignment.bottomCenter,
+              alignment: Alignment.topCenter,
               children: [
                 Positioned(
-                  bottom: 0,
+                  bottom: 40,
                   left: MediaQuery.of(context).size.width * 0.2 - 60, // Increase spacing
                   child: _buildTopThreeCircle(2, Colors.grey[300]!, 30, borderColor: Color(0xFFC0C0C0)), // Silver border
                 ),
                 Positioned(
-                  bottom: 30,
+                  bottom: 70,
                   child: _buildTopThreeCircle(1, Colors.grey[300]!, 35, borderColor: Color(0xFFFFD700), isCrowned: true), // Gold border
                 ),
                 Positioned(
-                  bottom: 0,
+                  bottom: 40,
                   right: MediaQuery.of(context).size.width * 0.2 - 60, // Increase spacing
                   child: _buildTopThreeCircle(3, Colors.grey[300]!, 30, borderColor: Color(0xFFCD7F32)), // Bronze border
                 ),
@@ -176,10 +181,6 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                       SizedBox(width: 4),
-                      Text(
-                        'Punten',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
                     ],
                   ),
                 );
@@ -189,25 +190,30 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
         ],
       ),
       bottomNavigationBar: Container(
+        margin: EdgeInsets.only(bottom: 10, right: 20, left: 20),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+            bottomLeft: Radius.circular(30.0), 
+            bottomRight: Radius.circular(30.0),
           ),
           boxShadow: [
             BoxShadow(
               color: Colors.black.withOpacity(1),
-              spreadRadius: 2,
-              blurRadius: 5,
+              spreadRadius: 1,
+              blurRadius: 2,
               offset: Offset(0, 3),
             ),
           ],
         ),
         child: ClipRRect(
           borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+            bottomLeft: Radius.circular(30.0), 
+            bottomRight: Radius.circular(30.0), 
           ),
           child: BottomNavigationBar(
             items: const <BottomNavigationBarItem>[
@@ -234,61 +240,63 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
                 case 0:
                   Navigator.pushNamed(context, '/homepage');
                   break;
-                case 1:
-                  Navigator.pushNamed(context, '/leaderboard');
-                  break;
-                case 2:
-                  Navigator.pushNamed(context, '/friends');
-                  break;
-                default:
-                  break;
-              }
-            },
-          ),
-        ),
-      ),
+          case 1:
+            Navigator.pushNamed(context, '/leaderboard');
+            break;
+          case 2:
+            Navigator.pushNamed(context, '/friends');
+            break;
+          default:
+            break;
+        }
+      },
+    ),
+  ),
+),
     );
-  }
+}
 
-  Widget _buildTopThreeCircle(int position, Color circleColor, double size, {bool isCrowned = false, required Color borderColor}) {
-    return Column(
-      children: [
-        Stack(
-          alignment: Alignment.center,
-          children: [
-            CircleAvatar(
-              radius: size + 3,
-              backgroundColor: borderColor,
-              child: CircleAvatar(
-                radius: size,
-                backgroundColor: circleColor,
-                child: Icon(Icons.account_circle, size: size * 2),
+
+Widget _buildTopThreeCircle(int position, Color circleColor, double size, {bool isCrowned = false, required Color borderColor}) {
+  return Column(
+    children: [
+      SizedBox(height: 10), // Add space at the top
+      Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          CircleAvatar(
+            radius: size + 3,
+            backgroundColor: borderColor,
+            child: CircleAvatar(
+              radius: size,
+              backgroundColor: circleColor,
+              child: Icon(Icons.account_circle, size: size * 2),
+            ),
+          ),
+          if (isCrowned)
+            Positioned(
+              top: -10,
+              child: Text(
+                '👑',
+                style: TextStyle(fontSize: 24),
               ),
             ),
-            if (isCrowned)
-              Positioned(
-                top: -10,
-                child: Text(
-                  '👑',
-                  style: TextStyle(fontSize: 24),
-                ),
-              ),
-          ],
-        ),
-        SizedBox(height: 5), // Adjust the gap here as needed
-        Column(
-          children: [
-            Text(
-              '{gebruikersnaam}',
-              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-            ),
-            Text(
-              '1001',
-              style: TextStyle(color: Colors.white),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
+        ],
+      ),
+      SizedBox(height: 5), // Add space between elements
+      Column(
+        children: [
+          Text(
+            '{gebruikersnaam}',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          Text(
+            '1001',
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    ],
+  );
+}
 }
