f6e8937efcec9fddf42f4e6ae734edf8206db6b9
LucasLubbers <lucas.lubbers@student.nhlstenden.com>
2024-05-22 21:55:31 +0200

    feat: made the top part of the information page

--- lib/informationpage.dart
+++ lib/informationpage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'profilepage.dart';
 
 class InformationPage extends StatefulWidget {
   @override
@@ -7,7 +6,6 @@ class InformationPage extends StatefulWidget {
 }
 
 class _InformationPageState extends State<InformationPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,65 +15,82 @@ class _InformationPageState extends State<InformationPage> {
         title: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image(
-              image: AssetImage('assets/walksmarterlogo.png'),
-              height: 40,
-              width: 40,
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/homepage');
+                    },
+                    child: Text(
+                      '< Ga Terug',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 11, 91, 14),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
             SizedBox(width: 8),
             Text(
               'Walk Smarter',
               style: TextStyle(fontSize: 14),
             ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 15),
-                  child: Text(
-                    '1000 Points',
-                    style: TextStyle(fontSize: 14),
-                  ),
+            Image(
+              image: AssetImage('assets/walksmarterlogo.png'),
+              height: 40,
+              width: 40,
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '{Monument naam}',
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                    height: 100,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300], // placeholder banner afbeelding monument
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "{Banner afbeelding van monument}",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/questionpage');
+                      },
+                      child: Text('Ga naar vraag'),
+                    ),
+                  ],
                 ),
               ),
             ),
           ],
-        ),
-        actions: [
-          IconButton(
-            iconSize: 40,
-            icon: Icon(Icons.account_circle),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => ProfilePage(),
-              ));
-            },
-          ),
-        ],
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1.0),
-          child: Container(
-            color: Colors.black,
-            height: 1.0,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/questionpage');
-                  },
-                  child: Text('Ga naar vraag'),
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
