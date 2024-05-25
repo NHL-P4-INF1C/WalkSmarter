cf44b05fc76af9e20b3c70d9ad36f2633deaafee
LucasLubbers <lucas.lubbers@student.nhlstenden.com>
2024-05-25 15:22:20 +0200

    feat: added top nav bar and question field

--- lib/questionpage.dart
+++ lib/questionpage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
+import 'package:flutter/widgets.dart';
+
 class TimerPainter extends CustomPainter {
   final Animation<double> animation;
   final Color backgroundColor;
@@ -37,7 +39,11 @@ class TimerPainter extends CustomPainter {
     TextPainter textPainter = TextPainter(
       text: TextSpan(
         text: '${(animation.value * 60).ceil()}',
-        style: TextStyle(color: Colors.black, fontSize: 20.0),
+        style: TextStyle(
+          color: Color.fromARGB(255, 9, 106, 46),
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
       ),
       textDirection: TextDirection.ltr,
     );
@@ -91,31 +97,100 @@ class _QuestionPageState extends State<QuestionPage> with SingleTickerProviderSt
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Your AppBar code
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: CustomPaint(
-                  size: Size(100, 100),
-                  painter: TimerPainter(
-                    animation: _controller,
-                    backgroundColor: Colors.green,
-                    color: Colors.grey,
+        automaticallyImplyLeading: false,
+        toolbarHeight: 50,
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/homepage');
+                    },
+                    child: Text(
+                      '< Ga Terug',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 9, 106, 46),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ),
               ),
-              SizedBox(height: 20),
-              // Other widgets in the body
-            ],
-          ),
+            ),
+            SizedBox(width: 8),
+            Text(
+              'Walk Smarter',
+              style: TextStyle(fontSize: 14),
+            ),
+            Image(
+              image: AssetImage('assets/walksmarterlogo.png'),
+              height: 40,
+              width: 40,
+            ),
+          ],
         ),
       ),
-      // Other parts of your Scaffold
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 80),
+            Container(
+              alignment: Alignment.centerLeft,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        Container(
+                          height: 200,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "{vraag}",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: -70, 
+                          left: (MediaQuery.of(context).size.width / 2) - 70,
+                          child: CustomPaint(
+                            size: Size(100, 100),
+                            painter: TimerPainter(
+                              animation: _controller,
+                              backgroundColor: Color.fromARGB(255, 9, 106, 46),
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      // bottom nav bar
     );
   }
 }
