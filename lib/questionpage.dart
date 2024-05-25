30500153c0b0d412a9145948fc37b88e569a982d
LucasLubbers <lucas.lubbers@student.nhlstenden.com>
2024-05-25 16:02:12 +0200

    feat: finished the questionpage by adding answers, submit button and bottom nav bar

--- lib/questionpage.dart
+++ lib/questionpage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-import 'package:flutter/widgets.dart';
-
 class TimerPainter extends CustomPainter {
   final Animation<double> animation;
   final Color backgroundColor;
@@ -67,6 +65,7 @@ class QuestionPage extends StatefulWidget {
 class _QuestionPageState extends State<QuestionPage> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   int duration = 60;
+  int? _selectedOption; 
 
   @override
   void initState() {
@@ -80,7 +79,7 @@ class _QuestionPageState extends State<QuestionPage> with SingleTickerProviderSt
 
     _controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
-        // logica mist nog
+        // logica mist nog wanneer timer voorbij is
       }
     });
 
@@ -181,8 +180,85 @@ class _QuestionPageState extends State<QuestionPage> with SingleTickerProviderSt
                         ),
                       ],
                     ),
+                    SizedBox(height: 30),
+                    Column(
+                      children: List.generate(3, (index) {
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 10),
+                          child: Stack(
+                            children: [
+                              Container(
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Container(
+                                        margin: EdgeInsets.all(4),
+                                        padding: EdgeInsets.all(8),
+                                        decoration: BoxDecoration(
+                                          color: _selectedOption == index
+                                              ? Color.fromARGB(155, 9, 106, 46)
+                                              : Colors.white,
+                                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                                        ),
+                                        child: Row(
+                                          children: [
+                                            Expanded(
+                                              child: Text(
+                                                'antwoord ${index + 1}',
+                                                style: TextStyle(fontSize: 16),
+                                              ),
+                                            ),
+                                            Radio<int>(
+                                              value: index,
+                                              groupValue: _selectedOption,
+                                              onChanged: (int? value) {
+                                                setState(() {
+                                                  _selectedOption = value;
+                                                });
+                                              },
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              if (_selectedOption == index)
+                                Positioned.fill(
+                                  child: Container(
+                                    margin: EdgeInsets.all(4),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                                      color: Color.fromARGB(0, 171, 209, 198),
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        );
+                      }),
+                    ),
                     SizedBox(height: 20),
-                    
+                    Container(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // logica om vraag te controleren mist nog
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all<Color>(const Color.fromARGB(255, 9, 106, 46)),
+                          foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                        ),
+                        child: Text(
+                          'Vraag controleren',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -190,7 +266,33 @@ class _QuestionPageState extends State<QuestionPage> with SingleTickerProviderSt
           ],
         ),
       ),
-      // bottom nav bar
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard),
+            label: 'Leaderboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Friends',
+          ),
+        ],
+
+        selectedItemColor: Color(int.parse('0xFF096A2E')),
+        onTap: (index) {
+        },
+      ),
+      bottomSheet: PreferredSize(
+        preferredSize: Size.fromHeight(1.0),
+        child: Container(
+          color: Colors.black,
+          height: 1.0,
+        ),
+      ),
     );
   }
 }
