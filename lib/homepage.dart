d1cca88d7cdd7229342698f3832020a2915565fd
bryan potze <bryan.potze@student.nhlstenden.com>
2024-05-27 09:40:21 +0200

    feat: completed the front end of home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,127 +7,121 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        toolbarHeight: 50,
-        title: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image(
-              image: AssetImage('assets/walksmarterlogo.png'),
-              height: 40,
-              width: 40,
-            ),
-            SizedBox(width: 8),
-            Text(
-              'Walk Smarter',
-              style: TextStyle(fontSize: 14),
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 15),
-                  child: Text(
-                    '1000 Points',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(
-            iconSize: 40,
-            icon: Icon(Icons.account_circle),
-            onPressed: () {
-              Navigator.pushNamed(context, '/profilepage');
-            },
-          ),
-        ],
-      ),
-      body: Column(
+      body: Stack(
         children: [
-          Center(
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.86,
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(
-                    color: Colors.black,
-                    width: 1,
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.white,
+            child: Center(
+              child: Text('Google Maps Widget Here'),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: PreferredSize(
+              preferredSize: Size.fromHeight(80.0),
+              child: AppBar(
+                automaticallyImplyLeading: false,
+                toolbarHeight: 50,
+                title: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage('assets/walksmarterlogo.png'),
+                      height: 40,
+                      width: 40,
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      'Walk Smarter',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 15),
+                          child: Text(
+                            '1000 Points',
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
                   ),
                 ),
               ),
-              child: Center(
-                child: Text('Google Maps Widget Here'),
+            ),
+          ),
+          Positioned(
+            left: 20,
+            right: 20,
+            bottom: 10,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30.0),
+                border: Border.all(
+                  color: Color(0xFF096A2E),
+                  width: 2.0,
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                child: BottomNavigationBar(
+                  items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.map),
+                      label: 'Map',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.leaderboard),
+                      label: 'Leaderboard',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.group),
+                      label: 'Friends',
+                    ),
+                  ],
+                  selectedItemColor: Color(0xFF096A2E),
+                  currentIndex: _selectedIndex,
+                  onTap: (index) {
+                    setState(() {
+                      _selectedIndex = index;
+                    });
+                    switch (index) {
+                      case 0:
+                        Navigator.pushNamed(context, '/homepage');
+                        break;
+                      case 1:
+                        Navigator.pushNamed(context, '/leaderboard');
+                        break;
+                      case 2:
+                        Navigator.pushNamed(context, '/friends');
+                        break;
+                      default:
+                        break;
+                    }
+                  },
+                ),
               ),
             ),
           ),
         ],
-      ),
-  bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            color: Colors.white, 
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(1), 
-                spreadRadius: 2,
-                blurRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
-          ),
-          child: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.map),
-                label: 'Map',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.leaderboard),
-                label: 'Leaderboard',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.group),
-                label: 'Friends',
-              ),
-            ],
-            selectedItemColor: Color(0xFF096A2E),
-            currentIndex: _selectedIndex,
-            onTap: (index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-              switch (index) {
-                case 0:
-                  Navigator.pushNamed(context, '/homepage');
-                  break;
-                case 1:
-                  Navigator.pushNamed(context, '/leaderboard');
-                  break;
-                case 2:
-                  Navigator.pushNamed(context, '/friends');
-                  break;
-                default:
-                  break;
-              }
-            },
-          ),
-        ),
       ),
     );
   }
