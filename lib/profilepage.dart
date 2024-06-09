c1c88c26383e5ec2c549d56b834d213e0887690a
Lars Kuijer <146839163+larsk1201@users.noreply.github.com>
2024-06-09 14:34:01 +0200

    feat: updated appbar in profile page

--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -87,45 +87,76 @@ class _ProfilePageState extends State<ProfilePage> {
     return friendNames;
   }
 
+  Future<String> fetchPoints() async {
+    try {
+      final response = await pb.collection('users').getOne(pb.authStore.model['id'].toString());
+      return response.data['points'].toString();
+    } catch (error) {
+      print('Error: $error');
+      return 'Err';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 245, 243, 243),
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60.0),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          toolbarHeight: 50,
-          title: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image(
-                image: AssetImage("assets/walksmarterlogo.png"),
-                height: 40,
-                width: 40,
-              ),
-              SizedBox(width: 8),
-              Text(
-                "Walk Smarter",
-                style: TextStyle(fontSize: 14),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 15),
-                    child: Text(
-                      "1001 Punten",
-                      style: TextStyle(fontSize: 14),
-                    ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        toolbarHeight: 50,
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage('assets/walksmarterlogo.png'),
+              height: 40,
+              width: 40,
+            ),
+            SizedBox(width: 8),
+            Text(
+              'Walk Smarter',
+              style: TextStyle(fontSize: 14),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: FutureBuilder<String>(
+                    future: fetchPoints(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return Text(
+                          'Error',
+                          style: TextStyle(fontSize: 14),
+                        );
+                      } else if (snapshot.hasData) {
+                        return Text(
+                          '${snapshot.data} Points',
+                          style: TextStyle(fontSize: 14),
+                        );
+                      } else {
+                        return Text(
+                          '0 Points',
+                          style: TextStyle(fontSize: 14),
+                        );
+                      }
+                    },
                   ),
                 ),
               ),
-            ],
-          ),
-          actions: [
-            Padding(
-              padding: EdgeInsets.only(right: 10.0),
+            ),
+          ],
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/profilepage');
+              },
               child: CircleAvatar(
                 radius: 23,
                 backgroundImage: _profilePicture.startsWith("http")
@@ -133,13 +164,13 @@ class _ProfilePageState extends State<ProfilePage> {
                   : AssetImage("assets/standardProfilePicture.png") as ImageProvider,
               ),
             ),
-          ],
-          backgroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(30),
-              bottomRight: Radius.circular(30),
-            ),
+          ),
+        ],
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
           ),
         ),
       ),
