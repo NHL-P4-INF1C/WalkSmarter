ed595cef96159ab93ab7cf146469c2add35f1294
Bram Suurd <contact@bramsuurd.nl>
2024-06-13 10:40:48 +0200

    chore: Componentise the bottom bar to only have to use one component for easy changes

--- lib/friendprofilepage.dart
+++ lib/friendprofilepage.dart
@@ -419,57 +419,9 @@ class _FriendProfilePageState extends State<FriendProfilePage> {
           ),
         ),
       ),
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.only(bottom: 15.0, left: 15.0, right: 15.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30.0),
-            border: Border.all(
-              color: Color(0xFF096A2E),
-              width: 2.0,
-            ),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(30.0),
-            child: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.map),
-                  label: "Map",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.leaderboard),
-                  label: "Leaderboard",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.group),
-                  label: "Friends",
-                ),
-              ],
-              selectedItemColor: Color.fromARGB(255, 119, 120, 119),
-              currentIndex: 1,
-              onTap: (index) {
-                setState(() {
-                  currentIndex = index;
-                  switch (index) {
-                    case 0:
-                      Navigator.pushNamed(context, '/homepage');
-                      return;
-                    case 1:
-                      Navigator.pushNamed(context, '/leaderboard');
-                      return;
-                    case 2:
-                      Navigator.pushNamed(context, '/friendspage');
-                      return;
-                    default:
-                      return;
-                  }
-                });
-              },
-            ),
-          ),
-        ),
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: currentIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
